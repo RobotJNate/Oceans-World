f6a3118ba6a11d42ab0aac498aa4f19f27af441c
RobotJNate <136011436+RobotJNate@users.noreply.github.com>
2026-02-19 13:46:51 -0800

    Update ow_engine.h

--- ow_engine.h
+++ ow_engine.h
@@ -8,14 +8,13 @@ public:
     void run();
     void shutdown();
 
-    void drawRect(float x, float y, float width, float height, int r, int g, int b);
 private:
     void processInput();
     void update(float deltaTime);
     void render();
 
     bool running = false;
-    OW_Window window;
+    OW_Window window; // window layer
 
     // rectangle test
     float rectX = 100.0f;
@@ -23,4 +22,3 @@ private:
     float rectWidth = 50.0f;
     float rectHeight = 50.0f;
 };
-
